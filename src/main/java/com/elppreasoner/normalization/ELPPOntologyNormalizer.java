1d7c04be7b51feae4041901be1a4d30f2cb63d5b
Danilo Esposito <danilo.esposito2016@gmail.com>
2024-07-07 17:41:02 +0200

    feat/normalization: All normalization rules added, normalizer completed. [feature completed, needs to be tested]

--- src/main/java/com/elppreasoner/normalization/ELPPOntologyNormalizer.java
+++ src/main/java/com/elppreasoner/normalization/ELPPOntologyNormalizer.java
@@ -4,61 +4,137 @@ import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Iterator;
 import java.util.List;
+import java.util.ListIterator;
 
 import org.semanticweb.owlapi.apibinding.OWLManager;
-import org.semanticweb.owlapi.model.IRI;
 import org.semanticweb.owlapi.model.OWLAxiom;
 import org.semanticweb.owlapi.model.OWLAxiomVisitor;
 import org.semanticweb.owlapi.model.OWLEquivalentClassesAxiom;
 import org.semanticweb.owlapi.model.OWLOntology;
 import org.semanticweb.owlapi.model.OWLOntologyCreationException;
-import org.semanticweb.owlapi.model.OWLOntologyManager;
 import org.semanticweb.owlapi.model.OWLSubClassOfAxiom;
 import org.semanticweb.owlapi.model.parameters.Imports;
 
+import com.elppreasoner.normalization.NormalizationUtilities.NormalizationRulesManager.NormalizationRule;
 import com.reasoner.normalization.OntologyNormalizer;
 
 public class ELPPOntologyNormalizer implements OntologyNormalizer, OWLAxiomVisitor {
 
+    private OWLOntology ontology;
     private OWLOntology normalizedOntology;
     private List<OWLAxiom> axiomsToNormalize;
 
-    public ELPPOntologyNormalizer() {}
+    public ELPPOntologyNormalizer() {
+        this.ontology = null;
+        this.normalizedOntology = null;
+        this.axiomsToNormalize = null;
+    }
 
     @SuppressWarnings("deprecation")
     @Override
     public OWLOntology normalize(OWLOntology ontology) {
-        // Create an empty ontology
-        IRI ontologyIRI = ontology.getOWLOntologyManager().getOntologyDocumentIRI(ontology);
-        List<OWLAxiom> ontologyTBoxAxioms = new ArrayList<>(
-            ontology.getTBoxAxioms(Imports.INCLUDED).size()
+        this.ontology = ontology;
+        this.normalizedOntology = null;
+        this.axiomsToNormalize = null;
+
+        // Create an empty ontology and an empty list of eventual axioms to normalize
+        this.axiomsToNormalize = new ArrayList<>(
+            this.ontology.getTBoxAxioms(Imports.INCLUDED).size()
         );
 
+        try {
+            this.normalizedOntology = OWLManager.createOWLOntologyManager().createOntology();
+        } catch (OWLOntologyCreationException e) {
+            throw new RuntimeException(e);
+        }
 
         // Analyze each axiom and put all non-normalized axioms in a list and all normalized axioms in the normalized ontology 
-        Iterator<OWLAxiom> it = ontology.axioms().iterator();
+        Iterator<OWLAxiom> it = this.ontology.axioms().iterator();
         while(it.hasNext()) {  
             OWLAxiom axiom = it.next();
             if (axiom instanceof OWLSubClassOfAxiom) {  // If the CBox has the form "C ⊑ D"
                 if (!NormalizationUtilities.isCBoxInNormalForm((OWLSubClassOfAxiom) axiom)) {
-                    axiomsToNormalize.add(axiom);
+                    this.axiomsToNormalize.add(axiom);
                 } else {
-                    normalizedOntology.add(axiom);
+                    this.normalizedOntology.add(axiom);
                 }
             } else if (axiom instanceof OWLEquivalentClassesAxiom) {  // If the CBox has the form "C1 ≡ ... ≡ Cn"
                 for (OWLSubClassOfAxiom subClassOfAxiom : ((OWLEquivalentClassesAxiom) axiom).asOWLSubClassOfAxioms()) {
                     if (!NormalizationUtilities.isCBoxInNormalForm(subClassOfAxiom)) {
-                        axiomsToNormalize.add(axiom);
+                        this.axiomsToNormalize.add(axiom);
                     } else {
-                        normalizedOntology.add(axiom);
+                        this.normalizedOntology.add(axiom);
                     }
                 }
             }
         }
 
-        // Normalize each non-normalized axiom
-        // TODO
+        // Normalize all non-normalized axioms. A do-while is necessary, because axioms can be made of more atomic non-normalized axioms, thus the list
+        // is updated and further iteration is needed
+        do {
+            ListIterator<OWLAxiom> iterator = this.axiomsToNormalize.listIterator();
+            while (iterator.hasNext()) {
+                OWLAxiom axiomToCheck = iterator.next();
+                iterator.remove();
+                axiomToCheck.accept(this);  // calls the visitor(this)'s visit() method, based on its type (OWLSubClassOfAxiom / OWLEquivalentClassesAxiom)
+            }
+        } while (!this.axiomsToNormalize.isEmpty());
 
-        return normalizedOntology;
+        this.ontology = null;
+        this.axiomsToNormalize = null;
+
+        return this.normalizedOntology;
+    }
+
+    @Override
+    public void visit(OWLSubClassOfAxiom axiom) {
+        NormalizationRule rule = NormalizationUtilities.NormalizationRulesManager.identifyNF(axiom);
+        Collection<OWLSubClassOfAxiom> normalizedSetOfAxioms = new ArrayList<>();
+        switch (rule) {
+            case NF2:
+                normalizedSetOfAxioms = NormalizationUtilities.NormalizationRulesManager.applyNF2(this.ontology, axiom);
+                break;
+            case NF3:
+                normalizedSetOfAxioms = NormalizationUtilities.NormalizationRulesManager.applyNF3(this.ontology, axiom);
+                break;
+            case NF4:
+                normalizedSetOfAxioms = NormalizationUtilities.NormalizationRulesManager.applyNF4(this.ontology, axiom);
+                break;
+            case NF5:
+                normalizedSetOfAxioms = NormalizationUtilities.NormalizationRulesManager.applyNF5(this.ontology, axiom);
+                break;
+            case NF6:
+                normalizedSetOfAxioms = NormalizationUtilities.NormalizationRulesManager.applyNF6(this.ontology, axiom);
+                break;
+            case NF7:
+                normalizedSetOfAxioms = NormalizationUtilities.NormalizationRulesManager.applyNF7(this.ontology, axiom);
+                break;
+            default:
+                break;
+        }
+
+        for (OWLSubClassOfAxiom a : normalizedSetOfAxioms) {
+            if (NormalizationUtilities.isCBoxInNormalForm(a)) {
+                this.normalizedOntology.addAxiom(a);
+            } else {
+                this.axiomsToNormalize.add(a);
+            }
+        }
+    }
+
+    /*
+     * If axiom has the form "C1 ≡ ... ≡ Cn", simply transform it into the equivalent SubClassOf axiom and just check if each subaxiom is in NF. 
+     */
+    @Override
+    public void visit(OWLEquivalentClassesAxiom axiom) {
+        Collection<OWLSubClassOfAxiom> subClassOfAxioms = axiom.asOWLSubClassOfAxioms();
+
+        for (OWLSubClassOfAxiom a : subClassOfAxioms) {
+            if (NormalizationUtilities.isCBoxInNormalForm(a)) {
+                this.normalizedOntology.addAxiom(a);
+            } else {
+                this.axiomsToNormalize.add(a);
+            }
+        }
     }
 }
--- src/main/java/com/elppreasoner/normalization/NormalizationUtilities.java
+++ src/main/java/com/elppreasoner/normalization/NormalizationUtilities.java
@@ -1,5 +1,7 @@
 package com.elppreasoner.normalization;
 
+import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
 
 import org.semanticweb.owlapi.model.OWLClass;
@@ -7,25 +9,29 @@ import org.semanticweb.owlapi.model.OWLClassExpression;
 import org.semanticweb.owlapi.model.OWLIndividual;
 import org.semanticweb.owlapi.model.OWLObjectIntersectionOf;
 import org.semanticweb.owlapi.model.OWLObjectOneOf;
+import org.semanticweb.owlapi.model.OWLObjectPropertyExpression;
 import org.semanticweb.owlapi.model.OWLObjectSomeValuesFrom;
+import org.semanticweb.owlapi.model.OWLOntology;
 import org.semanticweb.owlapi.model.OWLSubClassOfAxiom;
 
-import com.elppreasoner.normalization.normalization_rules.NormalizationRule;
+import uk.ac.manchester.cs.owl.owlapi.OWLDataFactoryImpl;
 
-public class NormalizationUtilities {
+public final class NormalizationUtilities {
     /* 
      * Legenda of concepts, from theory to OWL implementation: {
-     *     "bottom" (⊥)             ->      "owl:Nothing",
-     *     "top" (⊤)                ->      "owl:Thing",
-     *     "concept name"           ->      "OWLClass",
-     *     "individual name"        ->      "OWLIndividual",
-     *     "enum of individuals"    ->      "OWLObjectOneOf"    {a1}⊔{a2}⊔...⊔{an}
-     *     "C ⊑ D"                  ->      "OWLSubclassOf"
-     *     "C1 ≡ ... ≡ Cn"          ->      "EquivalentClasses"
+     *     "bottom" (⊥)                 <->      "owl:Nothing",
+     *     "top" (⊤)                    <->      "owl:Thing",
+     *     "concept name"               <->      "OWLClass",
+     *     "individual name | nominal"  <->      "OWLIndividual",
+     *     "C ⊑ D"                      <->      "OWLSubclassOf",
+     *     "C1 ≡ ... ≡ Cn"              <->      "EquivalentClasses",
+     *     "enum of individuals"        <->      "OWLObjectOneOf",  // {a1}⊔{a2}⊔...⊔{an}
+     *     "existential restriction ∃"  <->      "OWLObjectSomeValuesFrom",
+     *     "intersection ⊓"             <->      "OWLObjectIntersectionOf"
      * }
      * 
-     * Def. In our domain, an EL++ constraint box (CBox) is a a finite set of general concept inclusions (GCIs): C ⊑ D. In particular: C can be a
-     * basic concept (BC), while D can be either a basic concept (BC) or a bottom (⊥). [Role inclusions excluded]
+     * Def. In our domain, an EL++ constraint box (CBox) is a finite set of general concept inclusions (GCIs): C ⊑ D. In particular: C can be a
+     * basic concept (BC) or a nominal {a}, while D can be either a basic concept (BC), a nominal {a} or a bottom (⊥). [Role inclusions excluded]
      * 
      * Def. Given a CBox C, BC_C (read "basic concepts of C") denotes the smallest set of concept descriptions that contains the top concept ⊤, all
      * concept names used in C, and all concept descriptions of the form {a} or p(f1,..., fk) appearing in C.
@@ -46,7 +52,7 @@ public class NormalizationUtilities {
         return classExpression instanceof OWLIndividual;  // is it an individual name?
     }
 
-    // Given C ⊑ D, in this method we're considering C. In particular, C can be a basic concept (BC)
+    // Given C ⊑ D, in this method we're considering C. In particular, C can be a basic concept (BC) or a nominal {a}
     public static boolean isSubclassABasicConcept(OWLClassExpression subClassConcept) {
         return isConceptName(subClassConcept) ||        // is it a concept name?
             isIndividualName(subClassConcept) ||        // is it an individual name?
@@ -54,7 +60,7 @@ public class NormalizationUtilities {
             subClassConcept instanceof OWLObjectOneOf;  // is it an enumeration of individuals?
     }
 
-    // Given C ⊑ D, in this method we're considering D. In particular, D can be a basic concept (BC) or a bottom (⊥)
+    // Given C ⊑ D, in this method we're considering D. In particular, D can be a basic concept (BC), a nominal {a} or a bottom (⊥)
     public static boolean isSuperclassABasicConcept(OWLClassExpression superClassConcept) {
         return isConceptName(superClassConcept) ||        // is it a concept name?
             isIndividualName(superClassConcept) ||        // is it an individual name?
@@ -86,10 +92,213 @@ public class NormalizationUtilities {
         return false;  // CBox is not in normal form
     }
 
-    
 
-    // TODO
-    public static boolean isGenerated(OWLClass owlClass) {
-        return owlClass.toString().contains(NormalizationRule.GENERATED_CLASS);
+    public static class NormalizationRulesManager {
+        /*
+         * A CBox can be converted into Normal Form (NF) using the translation rules that follow in two phases:
+         *     1. exhaustively apply rules NF1 to NF4;
+         *     2. exhaustively apply rules NF5 to NF7.
+         * "Rule application" means that the concept inclusion on the left-hand side is replaced with the set of concept inclusions on the
+         * right-hand side.
+        */
+
+        // In this enum comments: C' and D' are NOT basic concepts and A denotes a new concept name
+        public enum NormalizationRule {
+            // NF1,  // role inclusion, not considered in our work
+            NF2,  // C ⊓ D' ⊑ E   -->   { D' ⊑ A, C ⊓ A ⊑ E }   * Please note that it can also be in the equivalent form D' ⊓ C
+            NF3,  //  ∃r.C' ⊑ D   -->   { C' ⊑ A, ∃r.A ⊑ D }
+            NF4,  //      ⊥ ⊑ D   -->   ∅
+            NF5,  //     C' ⊑ D'  -->   { C' ⊑ A, A ⊑ D' }
+            NF6,  //   B ⊑ ∃r.C'  -->   { B ⊑ ∃r.A, A ⊑ C' }
+            NF7  //  B ⊑ C ⊓ D   -->   { B ⊑ C, B ⊑ D }
+        }
+
+        public static NormalizationRule identifyNF(OWLSubClassOfAxiom axiom) {
+            OWLClassExpression subClass = axiom.getSubClass();
+            OWLClassExpression superClass = axiom.getSuperClass();
+    
+            if (isSuperclassABasicConcept(superClass)) {  // NF5,NF6 excluded
+                if (subClass instanceof OWLObjectIntersectionOf) {  // NF2
+                    return NormalizationRule.NF2;
+                }
+    
+                // NF2,NF5,NF6 excluded
+                if (subClass instanceof OWLObjectSomeValuesFrom &&
+                        !isSubclassABasicConcept(((OWLObjectSomeValuesFrom) subClass).getFiller())) {
+                    return NormalizationRule.NF3;
+                }
+            }
+            
+            if (isSubclassABasicConcept(subClass)) {  // NF6 or NF7
+                if (superClass instanceof OWLObjectSomeValuesFrom &&
+                        !isSuperclassABasicConcept(((OWLObjectSomeValuesFrom) superClass).getFiller())) {
+                    return NormalizationRule.NF6;
+                }
+                if (superClass instanceof OWLObjectIntersectionOf) {  // NF7
+                    return NormalizationRule.NF7;
+                }
+            }
+
+            if (subClass.isOWLNothing() && isSuperclassABasicConcept(superClass)) {  // NF4
+                return NormalizationRule.NF4;
+            }
+    
+            // subClass and superClass cannot be top (⊤ <-> owl:Thing), a top object/data property or a rdfs:literal
+            if (subClass.isTopEntity() || superClass.isTopEntity()) {
+                throw new IllegalArgumentException("Subclass and superclass cannot be top (owl:Thing), a top object/data property or a rdfs:literal.");
+            }
+    
+            // subClass and superClass are not basic concepts (only NF5 remains)
+            return NormalizationRule.NF5;
+        }
+
+        // the new concept name A
+        public static OWLClass generateOWLClass(OWLOntology ontology, OWLClassExpression subclass, OWLClassExpression superclass) {
+            return (new OWLDataFactoryImpl()).getOWLClass(
+                ontology.getOWLOntologyManager().getOntologyDocumentIRI(ontology) +
+                "#GENERATED" +
+                subclass.toString().hashCode() +
+                superclass.toString().hashCode()
+                );
+        }
+
+        // C ⊓ D' ⊑ E   -->   { D' ⊑ A, C ⊓ A ⊑ E }   * Please note that it can also be in the equivalent form D' ⊓ C
+        public static Collection<OWLSubClassOfAxiom> applyNF2(OWLOntology ontology, OWLSubClassOfAxiom axiom) {
+            OWLClassExpression subClass = axiom.getSubClass();
+            OWLClassExpression superClass = axiom.getSuperClass();
+
+            if (superClass.isAnonymous()) {
+                throw new IllegalArgumentException("Superclass needs to be named (have an IRI). Please, note that class entities are named (they have an IRI) while class expressions are anonymous.");
+            }
+
+            List<OWLClassExpression> intersectionOf = ((OWLObjectIntersectionOf) subClass).getOperandsAsList();
+            OWLClassExpression left = intersectionOf.get(0);
+            OWLClassExpression right = intersectionOf.get(intersectionOf.size()-1);
+
+            OWLClass newClass = generateOWLClass(ontology, subClass, superClass);  // new concept name A
+            OWLDataFactoryImpl owlDataFactory = new OWLDataFactoryImpl();
+            List<OWLSubClassOfAxiom> normalizedAxioms = new ArrayList<>();
+
+            if (!isSubclassABasicConcept(left)) {  // if left operand of the intersection is not a basic concept
+                normalizedAxioms.add(owlDataFactory.getOWLSubClassOfAxiom(left, newClass));                           // C ⊑ A                  
+                normalizedAxioms.add(                                                                                 // D' ⊓ A ⊑ E
+                    owlDataFactory.getOWLSubClassOfAxiom(owlDataFactory.getOWLObjectIntersectionOf(newClass, right),
+                    superClass
+                ));
+            } else if (!isSubclassABasicConcept(right)) {  // if right operand of the intersection is not a basic concept
+                normalizedAxioms.add(owlDataFactory.getOWLSubClassOfAxiom(right, newClass));                          // D' ⊑ A
+                normalizedAxioms.add(                                                                                 // C ⊓ A ⊑ E
+                    owlDataFactory.getOWLSubClassOfAxiom(owlDataFactory.getOWLObjectIntersectionOf(left, newClass),
+                    superClass
+                ));
+            } else {  // if both operands of the intersection are basic concepts (already normalized)
+                return null;  // No-op: should never enter this branch
+            }
+
+            return normalizedAxioms;
+        }
+
+        // ∃r.C' ⊑ D   -->   { C' ⊑ A, ∃r.A ⊑ D }
+        public static Collection<OWLSubClassOfAxiom> applyNF3(OWLOntology ontology, OWLSubClassOfAxiom axiom) {
+            OWLClassExpression subClass = axiom.getSubClass();
+            OWLClassExpression superClass = axiom.getSuperClass();
+
+            if (superClass.isAnonymous()) {
+                throw new IllegalArgumentException("Superclass needs to be named (have an IRI). Please, note that class entities are named (they have an IRI) while class expressions are anonymous.");
+            }
+
+            OWLObjectSomeValuesFrom existentialCE = (OWLObjectSomeValuesFrom) subClass;
+            OWLObjectPropertyExpression existentialCEProperty = existentialCE.getProperty();
+            OWLClassExpression filler = existentialCE.getFiller();
+            if (!filler.isAnonymous() || filler.isTopEntity()) {
+                throw new IllegalArgumentException("The class expression '" + filler.toString() + "' cannot be named or a top entity (top (owl:Thing), a top object/data property or a rdfs:literal).");
+            }
+            
+            OWLClass newClass = generateOWLClass(ontology, subClass, superClass);  // new concept name A
+            OWLDataFactoryImpl owlDataFactory = new OWLDataFactoryImpl();
+            List<OWLSubClassOfAxiom> normalizedAxioms = new ArrayList<>();
+
+            normalizedAxioms.add(owlDataFactory.getOWLSubClassOfAxiom(filler, newClass));  // C' ⊑ A
+            normalizedAxioms.add(owlDataFactory.getOWLSubClassOfAxiom(                     // ∃r.A ⊑ D
+                owlDataFactory.getOWLObjectSomeValuesFrom(existentialCEProperty, newClass),
+                superClass
+            ));
+
+            return normalizedAxioms;
+        }
+
+        // ⊥ ⊑ D   -->   ∅
+        public static Collection<OWLSubClassOfAxiom> applyNF4(OWLOntology ontology, OWLSubClassOfAxiom axiom) {
+            List<OWLSubClassOfAxiom> normalizedAxioms = new ArrayList<>();
+
+            return normalizedAxioms;  // ∅ is treated as an empty list and not as a null value; so, the list exists, but there are 0 elements in it
+        }
+
+        // C' ⊑ D'  -->   { C' ⊑ A, A ⊑ D' }
+        public static Collection<OWLSubClassOfAxiom> applyNF5(OWLOntology ontology, OWLSubClassOfAxiom axiom) {
+            OWLClassExpression subClass = axiom.getSubClass();
+            OWLClassExpression superClass = axiom.getSuperClass();
+
+            OWLClass newClass = generateOWLClass(ontology, subClass, superClass);  // new concept name A
+            OWLDataFactoryImpl owlDataFactory = new OWLDataFactoryImpl();
+            List<OWLSubClassOfAxiom> normalizedAxioms = new ArrayList<>();
+
+            normalizedAxioms.add(owlDataFactory.getOWLSubClassOfAxiom(subClass, newClass));    // C' ⊑ A
+            normalizedAxioms.add(owlDataFactory.getOWLSubClassOfAxiom(newClass, superClass));  // A ⊑ D'
+
+            return normalizedAxioms;
+        }
+
+        // B ⊑ ∃r.C'  -->   { B ⊑ ∃r.A, A ⊑ C' }
+        public static Collection<OWLSubClassOfAxiom> applyNF6(OWLOntology ontology, OWLSubClassOfAxiom axiom) {
+            OWLClassExpression subClass = axiom.getSubClass();
+            OWLClassExpression superClass = axiom.getSuperClass();
+
+            if (subClass.isAnonymous()) {
+                throw new IllegalArgumentException("Subclass needs to be named (have an IRI). Please, note that class entities are named (they have an IRI) while class expressions are anonymous.");
+            }
+
+            OWLObjectSomeValuesFrom existentialCE = (OWLObjectSomeValuesFrom) superClass;
+            OWLObjectPropertyExpression existentialCEProperty = existentialCE.getProperty();
+            OWLClassExpression filler = existentialCE.getFiller();
+            if (!filler.isAnonymous() || filler.isTopEntity()) {
+                throw new IllegalArgumentException("The class expression '" + filler.toString() + "' cannot be named or a top entity (top (owl:Thing), a top object/data property or a rdfs:literal).");
+            }
+
+            OWLClass newClass = generateOWLClass(ontology, subClass, superClass);  // new concept name A
+            OWLDataFactoryImpl owlDataFactory = new OWLDataFactoryImpl();
+            List<OWLSubClassOfAxiom> normalizedAxioms = new ArrayList<>();
+
+            normalizedAxioms.add(owlDataFactory.getOWLSubClassOfAxiom(                       // B ⊑ ∃r.A
+                subClass,
+                owlDataFactory.getOWLObjectSomeValuesFrom(existentialCEProperty, newClass))
+            );
+            normalizedAxioms.add(owlDataFactory.getOWLSubClassOfAxiom(newClass, filler));    // A ⊑ C'
+
+            return normalizedAxioms;
+        }
+
+        // B ⊑ C ⊓ D   -->   { B ⊑ C, B ⊑ D }
+        public static Collection<OWLSubClassOfAxiom> applyNF7(OWLOntology ontology, OWLSubClassOfAxiom axiom) {
+            OWLClassExpression subClass = axiom.getSubClass();
+            OWLClassExpression superClass = axiom.getSuperClass();
+
+            if (subClass.isAnonymous()) {
+                throw new IllegalArgumentException("Subclass needs to be named (have an IRI). Please, note that class entities are named (they have an IRI) while class expressions are anonymous.");
+            }
+
+            List<OWLClassExpression> intersectionOf = ((OWLObjectIntersectionOf) superClass).getOperandsAsList();
+            OWLClassExpression left = intersectionOf.get(0);
+            OWLClassExpression right = intersectionOf.get(intersectionOf.size()-1);
+
+
+            OWLDataFactoryImpl owlDataFactory = new OWLDataFactoryImpl();
+            List<OWLSubClassOfAxiom> normalizedAxioms = new ArrayList<>();
+
+            normalizedAxioms.add(owlDataFactory.getOWLSubClassOfAxiom(subClass, left));   // B ⊑ C
+            normalizedAxioms.add(owlDataFactory.getOWLSubClassOfAxiom(subClass, right));  // B ⊑ D
+
+            return normalizedAxioms;
+        }
     }
 }
