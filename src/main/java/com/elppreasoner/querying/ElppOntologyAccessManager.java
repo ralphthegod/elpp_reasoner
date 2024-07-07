85017207e4b99bc0d1d8c7d378ac92c0487bd338
Raffaele Di Maso <rafdimaso@gmail.com>
2024-07-08 01:12:16 +0200

    refactor ElppOntologyAccessManager to extend OntologyAccessManager

---
+++ src/main/java/com/elppreasoner/querying/ElppOntologyAccessManager.java
@@ -0,0 +1,14 @@
+package com.elppreasoner.querying;
+
+import org.semanticweb.owlapi.model.OWLOntology;
+
+import com.reasoner.querying.OntologyAccessManager;
+
+public class ElppOntologyAccessManager extends OntologyAccessManager{
+
+    public ElppOntologyAccessManager(OWLOntology ontology) {
+        super(ontology);
+    }
+
+    
+}
