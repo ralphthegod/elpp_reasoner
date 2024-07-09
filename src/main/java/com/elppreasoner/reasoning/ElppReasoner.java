e7e9e0d8d96b544617d9b7927fc9a6857c9c5ad2
Raffaele Di Maso <rafdimaso@gmail.com>
2024-07-09 16:50:19 +0200

    add ElppReasoner class with inference rules and calculators

--- src/main/java/com/elppreasoner/reasoning/ElppReasoner.java
+++ src/main/java/com/elppreasoner/reasoning/ElppReasoner.java
@@ -1,8 +1,12 @@
 package com.elppreasoner.reasoning;
 
+import org.semanticweb.owlapi.model.OWLOntology;
 import org.semanticweb.owlapi.reasoner.InferenceType;
 
-import com.reasoner.querying.OntologyAccessManager;
+import com.elppreasoner.querying.rules.IntersectionSuperclassesInferenceRule;
+import com.elppreasoner.querying.rules.SubclassRoleExpansionInferenceRule;
+import com.elppreasoner.querying.rules.SuperclassRoleExpansionInferenceRule;
+import com.elppreasoner.querying.rules.ToldSuperclassesInferenceRule;
 import com.reasoner.reasoning.Reasoner;
 
 /**
@@ -15,8 +19,17 @@ public class ElppReasoner extends Reasoner {
      * Dependency injection is used to provide the ontology accessor.
      * @param ontologyAccessor
      */
-    public ElppReasoner(OntologyAccessManager ontologyAccessor) {
-        super(ontologyAccessor);
+    public ElppReasoner(OWLOntology ontology) {
+        super(ontology);
+
+        // Add inference rules
+        addInferenceRule(new ToldSuperclassesInferenceRule()); // CR1
+        addInferenceRule(new IntersectionSuperclassesInferenceRule()); // CR2       
+        addInferenceRule(new SubclassRoleExpansionInferenceRule()); // CR3
+        addInferenceRule(new SuperclassRoleExpansionInferenceRule()); // CR4
+        // CR5 and CR6 do not require inference rules
+
+        // Add inference calculators
         addInferenceCalculator(InferenceType.CLASS_HIERARCHY, this::computeClassHierarchy);
     }
 
