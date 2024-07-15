4a68a89bff6fcf71d27c9b2c643453f50655dbc4
RalphTheCreator <rafdimaso@gmail.com>
2024-07-15 14:53:00 +0200

    feat/saturation: initialize ContextAccessManager with ontology rules and entities

--- src/main/java/com/reasoner/saturation/ContextAccessManager.java
+++ src/main/java/com/reasoner/saturation/ContextAccessManager.java
@@ -1,20 +1,46 @@
 package com.reasoner.saturation;
 
+import java.util.Collection;
 import java.util.HashMap;
 import java.util.Map;
-import java.util.Set;
 
 import org.semanticweb.owlapi.model.OWLEntity;
 
-import com.reasoner.querying.InferenceRule;
 import com.reasoner.querying.OntologyAccessManager;
+import com.reasoner.reasoning.rules.InferenceRule;
 
 public class ContextAccessManager{
     
-    private final Map<Class<? extends InferenceRule>, ContextProvider> inferenceRuleContexts = new HashMap<>();
-    
+    private final Map<Class<? extends InferenceRule>, ContextProvider> inferenceRuleContextProviders = new HashMap<>();
+
     public void initialize(OntologyAccessManager ontologyAccessManager) {
-        Set<OWLEntity> signature = ontologyAccessManager.getOntology().getSignature();
-        //TODO: Implement this method
+        initializeContextProviders(ontologyAccessManager.getRules());
+        
+        ontologyAccessManager.getOntology().signature().forEach((entity) -> {
+            inferenceRuleContextProviders.forEach((rule, contextProvider) -> {
+                InferenceRuleContext context = null;
+                try{
+                    contextProvider
+                        .getInferenceRule()
+                        .getContextType()
+                        .getDeclaredConstructor(InferenceRule.class, OWLEntity.class)
+                        .newInstance(rule, entity);
+                }
+                catch(Exception e){
+                    e.printStackTrace();
+                }
+                contextProvider.addContext(entity, context);
+            });
+        });
     }
+
+    private void initializeContextProviders(Collection<InferenceRule> rules) {
+        inferenceRuleContextProviders.clear();
+
+        rules.forEach((rule) -> {
+            ContextProvider contextProvider = new ContextProvider(rule);
+            inferenceRuleContextProviders.put(rule.getClass(), contextProvider);
+        });
+    }
+
 }
