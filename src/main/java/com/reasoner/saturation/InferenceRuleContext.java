1419cd31f5eec91539b71bd4b87ee44f77c6bb56
RalphTheCreator <rafdimaso@gmail.com>
2024-07-15 14:49:43 +0200

    feat/saturation: InferenceRuleContext to abstract class and add isActiveContext field

--- src/main/java/com/reasoner/saturation/InferenceRuleContext.java
+++ src/main/java/com/reasoner/saturation/InferenceRuleContext.java
@@ -1,5 +1,30 @@
 package com.reasoner.saturation;
 
-public interface InferenceRuleContext {
-    public void applyRule();
+import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Set;
+import java.util.concurrent.atomic.AtomicBoolean;
+
+import org.semanticweb.owlapi.model.OWLClassExpression;
+import org.semanticweb.owlapi.model.OWLEntity;
+import org.semanticweb.owlapi.model.OWLSubClassOfAxiom;
+
+import com.reasoner.reasoning.rules.InferenceRule;
+
+public abstract class InferenceRuleContext {
+
+    private final OWLClassExpression entity;
+    private final InferenceRule inferenceRule;
+    
+    private final AtomicBoolean isActiveContext = new AtomicBoolean(false);
+
+    private final Queue<OWLSubClassOfAxiom> scheduledAxioms = new LinkedList<>();
+    private final Set<OWLSubClassOfAxiom> processedAxioms = new HashSet<>();
+
+    public InferenceRuleContext(InferenceRule inferenceRule, OWLEntity entity) {
+        this.inferenceRule = inferenceRule;
+        this.entity = entity.asOWLClass();
+    }
+
 }
