86cacb55ebedb629eda161f204d5be3ffde1ed48
Ralph <rafdimaso@gmail.com>
2024-07-29 15:50:14 +0200

    feat/inference-rule-contexts: Implement IntersectionSuperclassesIRContext class

--- src/main/java/com/elppreasoner/saturation/contexts/IntersectionSuperclassesIRContext.java
+++ src/main/java/com/elppreasoner/saturation/contexts/IntersectionSuperclassesIRContext.java
@@ -1,12 +1,21 @@
 package com.elppreasoner.saturation.contexts;
 
+import java.util.HashSet;
 import java.util.Map;
 import java.util.Set;
 
+import static com.elppreasoner.normalization.NormalizationUtilities.isSubclassABasicConcept;
+import static com.elppreasoner.normalization.NormalizationUtilities.isSuperclassABasicConcept;
+
+import org.openrdf.model.vocabulary.OWL;
+import org.semanticweb.owlapi.apibinding.OWLManager;
+import org.semanticweb.owlapi.model.OWLClass;
 import org.semanticweb.owlapi.model.OWLClassExpression;
+import org.semanticweb.owlapi.model.OWLDataFactory;
 import org.semanticweb.owlapi.model.OWLEntity;
 import org.semanticweb.owlapi.model.OWLSubClassOfAxiom;
 
+import com.github.jsonldjava.shaded.com.google.common.base.Objects;
 import com.reasoner.reasoning.rules.InferenceRule;
 import com.reasoner.saturation.InferenceRuleContext;
 
@@ -17,24 +26,58 @@ public class IntersectionSuperclassesIRContext extends InferenceRuleContext<OWLC
     }
 
     @Override
-    public void addProcessedAxiom(OWLSubClassOfAxiom axiom) {
-        throw new UnsupportedOperationException("Not supported yet.");
+    public boolean addProcessedAxiom(OWLSubClassOfAxiom axiom) {
+        if(!isTargetEntity(axiom)) {
+            throw new IllegalArgumentException("Axiom is not for the target entity");
+        }
+        return processedAxioms.add(axiom);
     }
 
     @Override
     protected boolean isTargetEntity(OWLSubClassOfAxiom axiom) {
-        throw new UnsupportedOperationException("Not supported yet.");
+        OWLClassExpression subClass = axiom.getSubClass();
+        OWLClassExpression superClass = axiom.getSuperClass();
+        final boolean check = (!isSubclassABasicConcept(subClass) || !isSuperclassABasicConcept(superClass)) 
+            || (!Objects.equal(subClass, getEntity()));
+        return !check;
     }
 
     @Override
     public boolean hasProcessedAxiom(OWLSubClassOfAxiom axiom) {
-        throw new UnsupportedOperationException("Not supported yet.");
+        return processedAxioms.contains(axiom);
     }
 
     @Override
-    public Set<OWLSubClassOfAxiom> compute() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'compute'");
+    public Set<OWLSubClassOfAxiom> compute(OWLSubClassOfAxiom axiom) {
+        OWLClassExpression superclass = axiom.getSuperClass();
+        OWLClassExpression subclass = axiom.getSubClass();
+        if(!getInferenceRule().getAxioms().containsKey(superclass)){
+            return new HashSet<>();
+        }
+        Set<OWLClassExpression> intersectionSuperclasses = getIntersectionSuperclasses(subclass, superclass);
+        OWLDataFactory factory = OWLManager.getOWLDataFactory();
+        Set<OWLSubClassOfAxiom> conclusions = new HashSet<>();
+        intersectionSuperclasses.forEach(
+            sup -> conclusions.add(factory.getOWLSubClassOfAxiom(subclass, sup))
+        );
+        return conclusions;
+    }
+
+    private Set<OWLClassExpression> getIntersectionSuperclasses(
+        OWLClassExpression subclass,
+        OWLClassExpression superclass
+    ){
+        OWLDataFactory factory = OWLManager.getOWLDataFactory();
+        Set<OWLClassExpression> superclasses = new HashSet<>();
+
+        for(OWLClassExpression sup : getInferenceRule().getAxioms().get(superclass).keySet()){
+            OWLSubClassOfAxiom newAxiom = factory.getOWLSubClassOfAxiom(subclass, sup);
+            if(!Objects.equal(sup, getEntity()) && !hasProcessedAxiom(newAxiom)){
+                continue;
+            }
+        }
+
+        return superclasses;
     }
 
 }
