254da2632f3dfed877699745d29885740f7b6dd1
Ralph <rafdimaso@gmail.com>
2024-07-29 15:53:49 +0200

    feat/inference-rule-contexts: Refactor InferenceRuleContext compute method to accept an axiom

--- src/main/java/com/reasoner/saturation/InferenceRuleContext.java
+++ src/main/java/com/reasoner/saturation/InferenceRuleContext.java
@@ -26,14 +26,14 @@ public abstract class InferenceRuleContext<S,T> {
     private boolean isInitialized = false;
 
     private final Queue<OWLSubClassOfAxiom> scheduledAxioms = new ConcurrentLinkedQueue<>();
-    private final Set<OWLSubClassOfAxiom> processedAxioms = new HashSet<>();
+    protected final Set<OWLSubClassOfAxiom> processedAxioms = new HashSet<>();
 
     public InferenceRuleContext(InferenceRule<S,T> inferenceRule, OWLEntity entity) {
         this.inferenceRule = inferenceRule;
         this.entity = entity.asOWLClass();
     }
 
-    public abstract void addProcessedAxiom(OWLSubClassOfAxiom axiom);
+    public abstract boolean addProcessedAxiom(OWLSubClassOfAxiom axiom);
     public abstract boolean hasProcessedAxiom(OWLSubClassOfAxiom axiom);
     protected abstract boolean isTargetEntity(OWLSubClassOfAxiom axiom);
     
@@ -41,7 +41,7 @@ public abstract class InferenceRuleContext<S,T> {
      * Apply the inference rule on context axioms.
      * @return {@code Set<OWLSubClassOfAxiom>} conclusions.
      */
-    public abstract Set<OWLSubClassOfAxiom> compute();
+    public abstract Set<OWLSubClassOfAxiom> compute(OWLSubClassOfAxiom axiom);
 
     public boolean scheduleAxiom(OWLSubClassOfAxiom axiom){
         if(!isTargetEntity(axiom)) return false;
